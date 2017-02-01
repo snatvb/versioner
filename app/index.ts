92525b156eb40c1ec0e36d433c08d5f328e7042d
snatvb <snatvb@ya.ru>
2017-02-01 17:55:11 +0300

    fix build number

--- app/index.ts
+++ app/index.ts
@@ -33,7 +33,6 @@ export default class App {
     private dirname: string;
 
     constructor (dirname: string) {
-        return console.log('aaaa');
         this.dirname = dirname;
         if (this.about()) {
             return;
